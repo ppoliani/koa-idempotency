0a8c4f8354a57379266bc2fe6bc927eb5daf5290
ppoliani <ppoliani@gmail.com>
2020-06-08 15:52:50 +0100

    implement checkStore

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const {createKey} = require('./lib/utils');
 const {getStoreProvider} = require('./lib/storeProviders');
 
+const checkStore = async (storeOptions, idempotencyKey, ctx, next) => {
+  const {path, body} = ctx.request;
+  const store = getStoreProvider(storeOptions);
+  const cachedResponse = store.get(createKey(path, body, idempotencyKey));
+
+  if(!cachedResponse) {
+    return await next();
+  }
+
+  const {statusCode, body, headers} = cachedResponse;
+  ctx.status = statusCode;
+  ctx.body = body;
+  res.set(headers);
+  res.set('X-Cache', 'HIT');
+}
+
 const idempotence = opts => async (ctx, next) => {
+  const {storeOptions} = opts;
   const idempotencyKey = ctx.request.header['Idempotency-Key'];
   
   if(!idempotencyKey) {
     return await next();
   }
 
-  const store = getStoreProvider(opts.store);
+  await checkStore(storeOptions, idempotencyKey, ctx);
+
+
 }
 
 
