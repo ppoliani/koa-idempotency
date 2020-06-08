8df90b72542bc69f060c5d636006e91459fad1c8
ppoliani <ppoliani@gmail.com>
2020-06-08 16:46:18 +0100

    add readme

--- test/idempotence.test.js
+++ test/idempotence.test.js
@@ -40,3 +40,39 @@ test('it should execute the request and store the response if there is no respon
 
   t.deepEqual(store.get(createKey(ctx.request.path, ctx.response.body, idempotencyKey)), response);
 });
+
+test('it should return the cached version corresponding to the given Idempotency-Key', async t => {
+  const idempotencyKey = '12345';
+  const nextSpy = sinon.spy();
+  const response =  {
+    headers: {headerKey: 'headerVal'},
+    status: 201,
+    body: {prop: 'prop2'}
+  }
+
+  const ctx = {
+    set: (key, val) => function() {
+      if(typeof key === 'object') {
+        this.response.header = val;
+      }
+      else {
+        this.response.header[key] = val;
+      }
+    },
+    request: {
+      path: '/pay',
+      body: {prop: 'prop1'},
+      header: {
+        'Idempotency-Key': idempotencyKey
+      }
+    },
+    response
+  }
+
+  await idempotence()(ctx, nextSpy);
+  await idempotence()(ctx, nextSpy);
+
+  t.deepEqual(ctx.status, response.status);
+  t.deepEqual(ctx.response.header, {...response.headers, 'X-Cache': 'HIT'});
+  t.deepEqual(ctx.body, response.body);
+});
